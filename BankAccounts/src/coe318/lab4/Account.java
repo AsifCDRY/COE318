d731b10a89ee18e5adef56adb1564b824e428482
AsifCDRY <147215511+AsifCDRY@users.noreply.github.com>
2023-10-06 20:14:35 -0400

    Add files via upload

---
+++ BankAccounts/src/coe318/lab4/Account.java
@@ -0,0 +1,54 @@
+package coe318.lab4;
+
+/**
+ *
+ * @author Asif Choudhury, 501111719
+ */
+public class Account {
+    String accName;
+    int accID;
+    double accBalance;
+    
+    public Account(String name, int number, double initialBalance){
+        accName = name;
+        accID = number;
+        accBalance = initialBalance;
+    }
+
+    String getName(){
+        return(accName);
+    }
+    
+    double getBalance(){
+        return(accBalance);
+    }
+
+    int getNumber(){
+        return(accID);
+    }
+
+    boolean deposit(double amount){
+        if(amount <= 0)
+            return false;
+        else{
+            accBalance += amount;
+            return true;
+        }
+    }
+
+    boolean withdraw(double amount){
+        if(amount <= 0 || amount > accBalance)
+            return false;
+        else{
+            accBalance -= amount;
+            return true;
+        }
+    }
+
+    @Override
+    public String toString() {//DO NOT MODIFY
+        return "(" + getName() + ", " + getNumber() + ", " +
+        String.format("$%.2f", getBalance()) + ")";
+    }
+    
+}
